ea0e3c8a5632c5ee9fb03fb750816dcd7f70a00b
Gulshan <arun123gulshan@gmail.com>
2023-07-16 01:26:34 +0530

    ready to go

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ import About from './About';
 const App = () => {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/Enrolment.js
@@ -0,0 +1,9 @@
+const Enrolment = () => {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>You are currently on the Enrolment page.</p>
+      </div>
+    );
+  };
+export default Enrolment;  
